a82ec691af68e19848c114a1c714556fed4d293c
mig0el787-arch <mig0el787@gmail.com>
2026-01-13 15:20:12 -0300

    Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,25 +1,7 @@
--- GHOUL HUB | Loader.lua
-print("Ghoul Hub Loader iniciado")
+print("Loader iniciou")
 
-local BASE_URL = "https://raw.githubusercontent.com/mig0el787-arch/Ghoul.hub/main/"
+local UI = loadstring(game:HttpGet(
+	"https://raw.githubusercontent.com/mig0el787-arch/Ghoul.hub/main/UI.lua"
+))()
 
--- Carregar Functions.lua
-local Functions = loadstring(game:HttpGet(BASE_URL .. "Functions.lua"))()
-
-if not Functions then
-	warn("Erro ao carregar Functions.lua")
-	return
-end
-
--- Carregar UI.lua
-local UI = loadstring(game:HttpGet(BASE_URL .. "UI.lua"))
-
-if not UI then
-	warn("Erro ao carregar UI.lua")
-	return
-end
-
--- Executar UI passando as funções
-UI(Functions)
-
-print("Ghoul Hub carregado com sucesso")
+print("UI carregada")
